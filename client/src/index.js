ba880c7a0068dba04dec7528c69a2a95c3c43c0a
Roberto <rpgrca@gmail.com>
2023-06-20 18:05:33 -0300

    update Switch to Routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Route } from 'react-router';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import Logout from './Pages/Logout';
 import Login from './Pages/Login';
 import FullProduct from './Pages/FullProduct';
@@ -14,18 +14,18 @@ import Flush from './Pages/Flush';
 import Default from './Pages/Default';
 
 ReactDOM.render(<BrowserRouter>
-    <Switch>
-        <Route exact path='/' component={App} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route path='/logout' component={Logout} />
-        <Route path='/category/:category' component={App} />
-        <Route path='/category' component={App} />
-        <Route path='/product/:code' component={FullProduct} />
-        <Route path='/cart' component={Cart} />
-        <Route path='/flush' component={Flush} />
-        <Route component={Default} />
-    </Switch>
+    <Routes>
+        <Route exact path='/' element={App} />
+        <Route path='/login' element={Login} />
+        <Route path='/register' element={Register} />
+        <Route path='/logout' element={Logout} />
+        <Route path='/category/:category' element={App} />
+        <Route path='/category' element={App} />
+        <Route path='/product/:code' element={FullProduct} />
+        <Route path='/cart' element={Cart} />
+        <Route path='/flush' element={Flush} />
+        <Route element={Default} />
+    </Routes>
 </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
